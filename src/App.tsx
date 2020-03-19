72f0d4feffed7f880b34842718d8560cf870bb85
Louis <emyhoang8899@gmail.com>
2020-03-19 14:37:43 -0700

    use axios to get data from thirdparty
    - import axios
    - add IPost interface and IState
    - use axios in componentDidMount() method
    - return and render data
    - add some css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,44 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-class App extends React.Component {
+interface IPost {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+interface IState {
+  posts: IPost[];
+}
+
+class App extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      posts: []
+    };
+  }
+
+  public componentDidMount() {
+    axios.get<IPost[]>('https://jsonplaceholder.typicode.com/posts').then(response => {
+      this.setState({ posts: response.data });
+    });
+  }
+
   public render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-            Learn React
-          </a>
-        </header>
+        <ul className='posts'>
+          {this.state.posts.map(post => (
+            <li key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
